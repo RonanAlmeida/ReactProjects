6f7c3ce28d1104d766f827f33fc9d0c387c82fff
RonanAlmeida <ronancalmeida@hotmail.com>
2020-08-22 11:20:47 -0400

    preparing to add backend API/axios

--- react-shop-app/src/App.js
+++ react-shop-app/src/App.js
@@ -1,29 +1,31 @@
 import React from "react";
-import HelloWorld from "./Components/HelloWorld";
 import CounterExample from "./Components/CounterExample";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./Views/Home";
+import About from "./Views/About";
 
 function App() {
   return (
     <div>
       <Router>
         <Header />
-        <HelloWorld name="Tom" />
-
+        <div className="p-3">
         <Switch>
           <Route exact path="/">
-            <h1 className="font-bold text-2xl">This is the Home Page</h1>
+            <Home />
           </Route>
           <Route path="/about">
-            <h1 className="font-bold text-2xl">About us</h1>
+            <About />
           </Route>
+          
         </Switch>
+
+        </div>
+
         <Footer />
-
       </Router>
-
     </div>
   );
 }
---
+++ react-shop-app/src/Views/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h1 className="font-bold text-2xl mb-3">About Us</h1>
+      <p>
+            ok dude lol xd lmao thwsi iafih ijefi snfisnfeinf
+      </p>
+    </div>
+  );
+}
+
+export default About;
